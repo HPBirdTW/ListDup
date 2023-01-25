020f51dc786be47de5e4eb9c0fdf300906da6365
HPBirdChen <59825429+HPBirdTW@users.noreply.github.com>
2023-01-25 15:35:32 +0800

    Update version to 1.05

--- Main.cpp
+++ Main.cpp
@@ -1026,7 +1026,7 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 
         if (ExeAct == ParamAct::None)
         {
-            WTmpStr =  L" ListDup.exe <parameters>          VER(1.04)\n";
+            WTmpStr =  L" ListDup.exe <parameters>          VER(1.05)\n";
             WTmpStr += L"  -d   <Directory>     : Set (Dest) Directory\n";
             WTmpStr += L"  -src <Directory>     : Set (Src) Directory\n";
             WTmpStr += L"  -listClear           : Proc ListClear Action\n";
